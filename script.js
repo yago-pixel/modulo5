35d754d8d864a0fd06441959b2ef82379fd95162
Yagol <yagolira02@hotmail.com>
2023-05-22 19:04:20 -0400

    added updanting script.js

--- script.js
+++ script.js
@@ -1,30 +1,90 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-var userFormEl = document.querySelector('#user-form');
-var languageButtonsEl = document.querySelector('#language-buttons');
-var nameInputEl = document.querySelector('#username');
-var repoContainerEl = document.querySelector('#repos-container');
-var repoSearchTerm = document.querySelector('#repo-search-term');
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    var printEvents
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+var daily = [
+  {
+  id: "0",
+  hour: "09",
+  time"09",
+  meridiem: "am",
+  remider: ""
+  },
+  {
+    id: "1",
+    hour: "10",
+    time"10",
+    meridiem: "am",
+    remider: ""
+  },
+  {
+    id: "2",
+    hour: "11",
+    time"11",
+    meridiem: "am",
+    remider: ""
+  },
+  {
+    id: "3",
+    hour: "12",
+    time"12",
+    meridiem: "pm",
+    remider: ""
+  },
+  {
+    id: "4",
+    hour: "01",
+    time"13",
+    meridiem: "pm",
+    remider: ""
+  },
+  {
+    id: "5",
+    hour: "02",
+    time"14",
+    meridiem: "pm",
+    remider: ""
+  },
+  {
+    id: "6",
+    hour: "03",
+    time"15",
+    meridiem: "pm",
+    remider: ""
+  },
+  {
+    id: "7",
+    hour: "04",
+    time"16",
+    meridiem: "pm",
+    remider: ""
+  },
+  {
+    id: "8",
+    hour: "05",
+    time"17",
+    meridiem: "pm",
+    remider: ""
+  },
+  {
+    id: "9",
+    hour: "06",
+    time"18",
+    meridiem: "pm",
+    remider: ""
+  },
+]
+
+function getHeaderDate(){
+  var currentHeaderDate = moment().format('dddd, MMMM Do');
+  $("currentDay").text(currentHeaderDate);
+}
+    
+function saveReminders(){
+  locakStorage.setItem("daily",Json.stringify(daily));
+}
+function displayReminders(){
+  daily.forEach(function (_thishour){
+    $(`#${_thishour.id}`).val(_thishour.remider);
+  })
+}
+
+function 
   
